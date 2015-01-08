ae1edd444c7e352f250b6f6933cb2a2cc5e8bd52
sunafrus <hugo@ewertcs10.doverbroecks.com>
2015-01-08 15:43:41 +0000

    Update
    sik

--- WaveDefenceProject/Assets/MeleeEnemy.cs
+++ WaveDefenceProject/Assets/MeleeEnemy.cs
@@ -5,6 +5,10 @@ public class MeleeEnemy : Enemy {
 
 	public Transform target;
 
+	public Collider2D targetCollider;
+
+	//bool inCollision = false;
+
 	void Start ()
 	{
 		enemyStats.Speed = 3;
@@ -15,36 +19,66 @@ public class MeleeEnemy : Enemy {
 	{
 		if (target == null)
 		{			
-			target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>();;
+			target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>();
 		}
 
 		bool targetIsRight = (target.position.x > transform.position.x) ? true : false;
 
 		bool nextToPlayer = (Vector3.Distance (transform.position, target.position) < 1f) ? true : false;
 
-		Debug.Log (rigidbody2D.velocity.x);
+		Debug.Log ("Enemy Speed: " + rigidbody2D.velocity.x);
+
+		Debug.Log ("Next to player?: " + nextToPlayer);
+
+		//Debug.Log ("In Collision?: " + inCollision);
 
 		//move towards the player
-		if (!nextToPlayer)
+		if (nextToPlayer == false)
 		{
 
-			if (targetIsRight)
-			{
-				rigidbody2D.velocity = new Vector2(enemyStats.Speed, rigidbody2D.velocity.y);
-			}
-			else
-			{
-				rigidbody2D.velocity = new Vector2(-enemyStats.Speed, rigidbody2D.velocity.y);
-			}
+			//if (inCollision == false)
+			//{
+				if (targetIsRight)
+				{
+					rigidbody2D.velocity = new Vector2(enemyStats.Speed, rigidbody2D.velocity.y);
+				}
+				else
+				{
+					rigidbody2D.velocity = new Vector2(-enemyStats.Speed, rigidbody2D.velocity.y);
+				}
+			//}
 
-			if (rigidbody2D.velocity.x < 0.3f && rigidbody2D.velocity.x > -0.3f)
+			if (rigidbody2D.velocity.x < 0.3f)
 			{
-				Debug.Log("Should be jumping");
-				rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, enemyStats.Speed);
+				Debug.Log("STAGE ONE");
+
+				if (rigidbody2D.velocity.x > -0.3f)
+				{
+					Debug.Log("STAGE TWO");
+					rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, enemyStats.Speed);
+				}
 			}
 
 		}
 
 	
 	}
+
+	/*void onCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Wall")
+		{
+			inCollision = true;
+			Debug.Log ("Entered collision");
+		}
+	}
+
+	void onCollisionExit2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Wall")
+		{
+			inCollision = false;
+			Debug.Log ("Exited collision");
+		}
+	}*/
 }
